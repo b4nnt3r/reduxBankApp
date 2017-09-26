50fea96c8b024b23e7d08e7fa8c4bf99c6bbedd9
Brandon Walden <brandonshanewalden@gmail.com>
2017-09-26 11:20:39 -0400

    switched lodash clonedeep to update to persist the state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import { USER_SELECTED, ACCOUNT_SELECTED, WITHDRAW_FUNDS } from "../actions/index";
 import userList from "../data/users";
-import _ from "lodash";
 
 const initialState = {
   users: userList(),
@@ -8,32 +7,47 @@ const initialState = {
   selectedAccount: null
 };
 let newState;
+
 const reducer = function(state = initialState, action) {
+
   switch (action.type) {
     case USER_SELECTED:
-      let userId = action.payload;
-
-      newState = _.cloneDeep(state);
-      newState.selectedUser = userList().find(element => {
-        return element._id === userId;
-      });
-      return newState;
+        return update(state, {
+            selectedUser: {
+                $set: action.payload
+            }
+        });
 
     case ACCOUNT_SELECTED:
-      newState = _.cloneDeep(state);
-      newState.selectedAccount = action.payload;
-      return newState;
+        return update(state, {
+            selectedAccount: {
+                $set: action.payload
+            }
+        });
 
     case WITHDRAW_FUNDS:
-      newState = _.cloneDeep(state);
+        const userIdx = state.users.findIndex(user => user._id === state.selectedUser._id);
+        const accountIdx = state.users[userIdx].accounts.findIndex(account => account.id === state.selectedAccount.id);
 
-      const account = newState.selectedAccount;
-      account.balance -= action.payload;
-      return newState;
+        return update(state, {
+            users: {
+                [userIdx]: {
+                    accounts: {
+                        [accountIdx]: {
+                            balance: {
+                                $apply: function(balance) {
+                                    return balance - action.payload
+                                }
+                            }
+                        }
+                    }
+                }
+            }
 
+        })
     default:
-      return state;
+        return state;
   }
-};
+}
 
 export default reducer;
